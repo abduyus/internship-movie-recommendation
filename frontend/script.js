a20e9f0a412c6457e758827fe9cf945b2d4a4302
yusuf <yusuf.abdurrasheed@outlook.com>
2025-01-02 10:28:21 +0000

    Add virtual environment files and update package metadata

--- frontend/script.js
+++ frontend/script.js
@@ -3,19 +3,11 @@
 const movieNameEl = document.querySelector(".movie-form");
 
 console.log(movieNameEl);
-
+console.log(encodeURIComponent("iron man"));
 const getRecommendedMovies = async function (movieName) {
-  const res = await fetch(
-    `http://127.0.0.1:5000/recommend?movie=${encodeURIComponent(movieName)}`
-  );
+  const res = await fetch(`http://127.0.0.1:8000/recommend?movie=${movieName}`);
   const data = await res.json();
   console.log(data);
 };
 
-getRecommendedMovies("iron man");
-
-app.get("/recommend", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  // your existing code
-});
+getRecommendedMovies("superman");
