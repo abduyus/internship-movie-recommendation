cff040a84353d82ca28ac935af0bef94703bbd4d
yusuf <yusuf.abdurrasheed@outlook.com>
2025-01-03 10:23:07 +0000

    Refactor movie search form and update fetch request to use movie_title parameter

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -1,20 +1,28 @@
 "use strict";
 
-const movieNameEl = document.querySelector(".movie-form");
+const movieFormEl = document.querySelector(".search-bar");
+const movieInputEl = document.querySelector(".movie-form");
 
-console.log(movieNameEl);
-console.log(encodeURIComponent("iron man"));
+movieFormEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const movieName = movieInputEl.value.trim();
+  if (movieName) {
+    getRecommendedMovies(movieName);
+  }
+});
+
 const getRecommendedMovies = async function (movieName) {
   try {
     const res = await fetch(
-      `http://127.0.0.1:5000/recommend?movie=${movieName}`
+      `http://127.0.0.1:5000/recommend?movie_title=${encodeURIComponent(
+        movieName
+      )}`
     );
-    if (!res.ok) throw new Error(`An error has occured: ${res.status}`);
+    if (!res.ok) throw new Error(`An error has occurred: ${res.status}`);
     const data = await res.json();
     console.log(data);
+    displayRecommendations(data);
   } catch (error) {
     console.error("Network error:", error);
   }
 };
-
-getRecommendedMovies("superman");
