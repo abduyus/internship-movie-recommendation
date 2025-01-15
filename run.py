ad6906d9deb1305acb61749476c60dded0d2fab6
yusuf <yusuf.abdurrasheed@outlook.com>
2025-01-15 20:47:49 +0000

    updating port

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from backend.app import app
+from waitress import serve
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug = True)
+    serve(app, host="0.0.0.0", port=5000)
