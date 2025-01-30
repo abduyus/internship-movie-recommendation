463eeaa868a5569d99c6b390c77a17256ed056b4
yusuf <yusuf.abdurrasheed@outlook.com>
2025-01-30 07:02:44 +0000

    fixing the genre filter

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:812b258ad022bfad38fb4620c2733863777dd29bfd7c54466c73f7b127ff2cea
-size 5033
+oid sha256:9552004ed1fd1db9d70728c92d4b03fdd1d5731246f6784adb166b6310cdc0aa
+size 5030
